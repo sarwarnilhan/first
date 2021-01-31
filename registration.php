6a3469de26fd86a06aedf80b8940fa1b9c99f237
nilhan sarwar <77848392+sarwarnilhan@users.noreply.github.com>
2021-01-31 17:29:06 +0600

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,63 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title> registration page</title>
+</head>
+<body>
+	<h1> My registration Page</h1>
+	
+
+
+
+	<form>
+		
+		<table>
+			<tr><td>First Name :</td>
+				<td> <input type="text" name="firstname"></td>
+			
+			</tr>
+			<tr> <td> Email :</td>
+				<td> <input type="text" name="email"></td>
+
+			</tr>
+			<tr>
+				<td> User Name :</td>
+				<td> <input type="text" name="uname"> </td>
+			</tr>
+			<tr>
+				<td> Password :</td>
+				<td> <input type="password" name="pwd"></td>
+			</tr>
+			<tr>
+				<td> Confirm Password :</td>
+				<td> <input type="password" name="pwd"></td>
+			</tr>
+			<tr> <td> Gender :
+			 <input type="radio" id="male" name="gender" value="male"> Male
+			 <input type="radio" id="male" name="gender" value="female"> Female
+			 <input type="radio" id="male" name="gender" value="other"> Other</td> 
+
+				
+			</tr>
+			<tr>
+				<td> Date of birth</td>
+				<td><input type="date" name="dob"></td>
+
+
+			</tr>
+
+			<tr> <td><input type="submit" name="submit">
+			<input type="reset" name="reset"></td></tr>
+			<tr>
+				
+
+
+			
+		</table>
+		
+	</form>
+
+</body>
+</html>
+
